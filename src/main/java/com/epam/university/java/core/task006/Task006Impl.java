aa0428aa79618e8525e73dc57e9d9b214c469ac3
Mariia Atanova <canticleofsacrifice@gmail.com>
2020-08-23 18:02:44 +0300

    feat: task006

---
+++ src/main/java/com/epam/university/java/core/task006/Task006Impl.java
@@ -0,0 +1,52 @@
+package com.epam.university.java.core.task006;
+
+import java.util.Collection;
+import java.text.DecimalFormat;
+
+
+public class Task006Impl implements Task006 {
+
+    @Override
+    public double resistance(Collection<Measurement> measurements) {
+
+        if (isArgumentInvalid(measurements)) {
+            throw new IllegalArgumentException();
+        }
+
+        double avgAmpere = 0;
+        double avgVoltage = 0;
+
+        for (Measurement measurement :
+                measurements) {
+            avgAmpere += measurement.getAmperage();
+            avgVoltage += measurement.getVoltage();
+        }
+
+        if (avgAmpere == 0) {
+            return 0;
+        }
+
+        avgAmpere /= measurements.size();
+        avgVoltage /= measurements.size();
+
+        double slopeNumerator = 0;
+        double slopeDenominator = 0;
+        for (Measurement measurement :
+                measurements) {
+            slopeNumerator += (measurement.getAmperage() - avgAmpere)
+                    * (measurement.getVoltage() - avgVoltage);
+            slopeDenominator += Math.pow((measurement.getAmperage() - avgAmpere), 2);
+        }
+
+        DecimalFormat df = new DecimalFormat("#.###");
+        double resistance = slopeNumerator / slopeDenominator;
+
+        return Double.parseDouble(df.format(resistance).replace(",", "."));
+
+    }
+
+    private boolean isArgumentInvalid(Collection<Measurement> argument) {
+        return argument == null;
+    }
+
+}
