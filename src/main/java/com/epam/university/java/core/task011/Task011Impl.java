86f4f61c60b46b45483fe81cceb755138fca6598
Mariia Atanova <canticleofsacrifice@gmail.com>
2020-08-24 20:37:31 +0300

    feat: task011 in progress

---
+++ src/main/java/com/epam/university/java/core/task011/Task011Impl.java
@@ -0,0 +1,74 @@
+package com.epam.university.java.core.task011;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Task011Impl implements Task011 {
+    @Override
+    public String getLastName(String[] collection) {
+
+        if (isArgumentInvalid(collection)) {
+            throw new IllegalArgumentException();
+        }
+
+        int inCircle = collection.length;
+        int index = 0;
+
+        while (true) {
+            if (inCircle == 1) {
+                return collection[0];
+            }
+            collection[index] = null;
+            moveArrayElements(collection, index, inCircle - 1);
+            inCircle--;
+            index = (index + 1) % inCircle;
+
+        }
+    }
+
+    private void moveArrayElements(String[] array, int startPosition, int endPosition) {
+
+        String firstElement = array[startPosition];
+        startPosition++;
+
+        if (endPosition + 1 - startPosition >= 0)
+            System.arraycopy(array, startPosition, array, startPosition - 1,
+                    endPosition + 1 - startPosition);
+        array[endPosition] = firstElement;
+
+    }
+
+
+    @Override
+    public String getLastName(ArrayList<String> collection) {
+
+        if (isArgumentInvalid(collection)) {
+            throw new IllegalArgumentException();
+        }
+
+        int index = 0;
+
+        while (collection.size() != 1) {
+            collection.remove(index);
+            index = (index + 1) % collection.size();
+        }
+
+        return collection.get(0);
+    }
+
+    @Override
+    public String getLastName(LinkedList<String> collection) {
+        return null;
+    }
+
+    private boolean isArgumentInvalid(String[] collection) {
+        return collection.length == 0;
+    }
+
+    private boolean isArgumentInvalid(Collection<String> collection) {
+        return collection.size() == 0;
+    }
+
+
+}
